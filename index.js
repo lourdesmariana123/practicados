4c4ed700121a2379f21213d2aaea9af6afd02955
mariana guanotuña <mariana199lourdes@outlook.com>
2023-02-18 22:06:11 -0500

    commit one

---
+++ index.js
@@ -0,0 +1,21 @@
+const fn = () => {
+    const miString = "soy un string!" // funcion local, el recudro rojo
+    let b = miString + " muy feliz"                                  
+    console.log(b)
+    console.log('hola mundo')
+    console.log('Hola mundo dos');
+    
+    // soy maBrian       
+    
+    
+    
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "chanchito feliz"
+
+    console.log(a)
+}
+
